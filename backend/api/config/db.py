3b633a5b8585c397abdf06e114e877e272defbcd
Marvin Goesmann <mgo81@uclive.ac.nz>
2018-09-11 11:58:18 +1200

    persistent db

--- backend/api/config/db.py
+++ backend/api/config/db.py
@@ -1,22 +1,36 @@
 #!flask/bin/python
 import pymysql.cursors
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='Sudoku',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+from DBUtils.PersistentDB import PersistentDB
 
-def query(sql, values=""):
-    try:
-        with connection.cursor() as cursor:
-            if values != "":
-                cursor.execute(sql, values)
-                result = cursor.fetchall()
-            else:
-                cursor.execute(sql)
-                result = cursor.fetchall()
-        connection.commit()
-    finally:
-        connection.close
+dbConn = None
+
+def connect_db():
+    return PersistentDB(
+        creator=pymysql,  # the rest keyword arguments belong to pymysql
+        user='root',
+        password='',
+        database='Sudoku',
+        autocommit=True,
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+
+
+def get_db():
+    global dbConn
+
+    if not dbConn:
+        dbConn = connect_db()
+    return dbConn.connection()
+
+def query(sql, values=None):
+    global dbConn
+
+    with get_db().cursor() as cursor:
+        if values:
+            cursor.execute(sql, values)
+            result = cursor.fetchall()
+        else:
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
     return result
